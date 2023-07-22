080d15a92b39110bfea8d4e0ccad1cb66f1bc628
minseo  jo <liging12@naver.com>
2023-07-22 23:33:07 +0900

    productList.html 상품 정보 링크 , 디폴트 상품 이미지 추가

--- src/main/java/demo/demo/controller/HomeController.java
+++ src/main/java/demo/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package demo.demo.controller;
 
 import demo.demo.domain.Product;
 import demo.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,20 +11,10 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-//@RequestMapping("")
+@RequiredArgsConstructor
 public class HomeController {
     private final ProductService productService;
-    @Autowired
-    public HomeController(ProductService productService) {
-        this.productService = productService;
-    }
 
-    /*
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-    */
     @GetMapping("/")
     private ModelAndView home() throws Exception {
         List<Product> products = productService.findLimitTwenty();
